4c70a22a4b0116e6f2b77437d61cd1437d91338d
David Jackson <altmail@jackson-wallace.net>
2015-09-12 16:36:56 -0500

    Added setup.py

---
+++ DESCRIPTION.rst
@@ -0,0 +1,7 @@
+code_monkey
+===========
+
+code_monkey is a tool to automate complicated refactoring for large Python
+projects. It builds a tree representing your project, then provides an easy API
+for querying nodes out of the tree. Once you've found the nodes you're looking
+for, you can generate changes and apply them to your whole project at once.
---
+++ setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='code_monkey',
+
+    version='0.0.1',
+
+    description='A Python refactoring/static analysis tool',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='',
+
+    # Author details
+    author='David Jackson',
+    author_email='altmail@jackson-wallace.net',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators'
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='development code_generation static_analysis',
+
+    packages=['code_monkey'],
+
+
+    install_requires=[
+        'wsgiref==0.1.2',
+        'astroid==1.1.1',
+        'logilab-common==0.61.0'
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': [
+            'coverage==3.7.1',
+            'nose==1.3.3'
+        ],
+    },
+
+)
